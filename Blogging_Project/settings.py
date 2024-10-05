5d04c127fdc49ae0ff6aa6bf964b58c9ce52168d
Wuor Bhang <uhuribhang211@gmail.com>
2024-10-05 22:21:08 +0300

    updates to files

--- Blogging_Project/settings.py
+++ Blogging_Project/settings.py
@@ -131,9 +131,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
-django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Ensure this directory exists
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
